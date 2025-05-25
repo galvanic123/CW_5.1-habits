ad7c74280534b10363a1605b7371f1a223f14850
Alexandr Savitsky <savitsky.alexandr@mail.ru>
2025-05-25 21:28:21 +0300

    cw_5.1 1

--- habits/admin.py
+++ habits/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from habits.models import Habit
+
+
+@admin.register(Habit)
+class HabitAdmin(admin.ModelAdmin):
+    """Админка модели привычка."""
+
+    list_display = (
+        "id",
+        "habit",
+        "owner",
+        "place",
+        "start_time",
+        "action",
+        "is_pleasant",
+        "related_habit",
+        "periodicity",
+        "remuneration",
+        "execution_time",
+        "is_published",
+    )
+    search_fields = ("habit",)
+    search_filter = ("habit",)
